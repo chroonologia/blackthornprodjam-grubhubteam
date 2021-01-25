6137a2694100cd8415faf78274a32d3219a0c3cb
KillerT800 <43156534+KillerT800@users.noreply.github.com>
2021-01-25 22:22:25 +0000

    Basic Movement
    UI Description still in work

---
+++ Assets/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public GameObject FloatText;
+    public Transform Collider;
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(FloatText != null)
+            {
+                Instantiate(FloatText, Collider.position, Collider.rotation);
+            }
+            Debug.Log("Player can now pick up this item!");
+        }
+    }
+}
